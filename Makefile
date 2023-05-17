76f008ab77e3b15b6e5b7a05559fb0f6060e0ca2
matheo <math.dumont.md@gmail.com>
2023-05-17 15:51:26 +0200

    change compil to static lib with files in lib,include and exec in src

--- Makefile
+++ Makefile
@@ -1,29 +1,37 @@
 # https://gl.developpez.com/tutoriel/outil/makefile/
 # https://makefiletutorial.com/#implicit-rules
-LIBS=-lGL -lX11 -lpthread -lXrandr -ldl -lglfw -lGLEW -lGLU
-SRC_DIR :=./src
-BUILD_DIR :=./build
-TARGET_EXEC := program
-CPP_FILES := $(shell find $(SRC_DIR) -name '*.cpp')
+# https://renenyffenegger.ch/notes/development/languages/C-C-plus-plus/GCC/create-libraries/index
+# https://stackoverflow.com/questions/2649334/difference-between-static-and-shared-libraries
+exec := ray_marching.cpp # or main.cpp
+LIB_NAME := lib.a
 
+LIB_DIR := ./lib
+SRC_DIR := ./src
+BUILD_DIR :=./build
+
+CPP_FILES := $(shell find $(LIB_DIR) -name '*.cpp')
 BUILD_CPP_FILES := $(CPP_FILES:%=$(BUILD_DIR)/%.o)
+
+LIBS=-lGL -lX11 -lpthread -lXrandr -ldl -lglfw -lGLEW -lGLU
 INCLUDES=./include
 STD=-std=c++17
 
+build_lib: $(BUILD_DIR)/$(LIB_NAME)
 
-$(BUILD_DIR)/$(TARGET_EXEC): $(BUILD_CPP_FILES)
-	echo $(CPP_FILES)
-	$(CXX) $(STD) $(BUILD_CPP_FILES) $(CPPFLAGS) $(CXXFLAGS) $(LIBS) -I$(INCLUDES)  -o $@ 
-
-$(BUILD_DIR)/%.cpp.o: %.cpp
-	mkdir -p $(dir $@)
-	$(CXX) $(STD) $(CPPFLAGS) $(CXXFLAGS) -I$(INCLUDES) -c $< -o $@ 
-
-build: $(BUILD_DIR)/$(TARGET_EXEC)
-
-run: $(BUILD_DIR)/$(TARGET_EXEC)
+run: $(BUILD_DIR)/$(exec).out
 	./$^
 
 clean: 
 	rm -rf $(BUILD_DIR)
+
+$(BUILD_DIR)/$(LIB_NAME): $(BUILD_CPP_FILES)
+	# $(CXX) $(STD) $(CPPFLAGS) $(CXXFLAGS) $(LIBS) -I$(INCLUDES) -o $(BUILD_DIR)/lib$(LIB_NAME) $^
+	ar rcs $@ $^ 
+
+$(BUILD_DIR)/$(exec).out: $(BUILD_DIR)/$(LIB_NAME) 
+	$(CXX) $(STD) $(SRC_DIR)/$(exec) -L$(BUILD_DIR) -l:$(LIB_NAME) -I$(INCLUDES) $(CPPFLAGS) $(CXXFLAGS) $(LIBS) -o $@
+
+$(BUILD_DIR)/%.cpp.o: %.cpp
+	mkdir -p $(dir $@)
+	$(CXX) $(STD) $(CPPFLAGS) $(CXXFLAGS) -I$(INCLUDES) -c $< -o $@ 
 
---
+++ src/ray_marching.cpp
@@ -0,0 +1,83 @@
+// include glew BEFORE glfw3
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include "viewer_core/shader.h"
+#include "viewer_core/shader_program.h"
+#include "viewer_core/viewer.h"
+
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (action == GLFW_PRESS)
+    {
+        switch (key)
+        {
+        case GLFW_KEY_ESCAPE:
+            glfwSetWindowShouldClose(window, true);
+            break;
+        case GLFW_KEY_Z:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            break;
+        case GLFW_KEY_X:
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+int main()
+{
+    vw::Viewer viewer{800, 600}; 
+    glfwSetKeyCallback(viewer.window, key_callback);
+    
+    std::vector quad {
+        // triangle 1
+        1.0f, 1.0f, 0.0f,
+        -1.0f, -1.0f, 0.0f,
+        -1.0f, 0.0f, 0.0f,
+        // triangle 2
+        1.0f, 1.0f, 0.0f,
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+    };
+    unsigned int VAO1;
+    glGenVertexArrays(1, &VAO1);
+    glBindVertexArray(VAO1);
+    
+    unsigned int VBO1;
+    glGenBuffers(1, &VBO1);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * quad.size(), quad.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);  
+    glEnableVertexAttribArray(0);
+
+    vw::Shader vertex_shader{"ray_marching.vs", GL_VERTEX_SHADER};
+    vertex_shader.load_and_compile();
+    vw::Shader fragment_shader{"ray_marching.fs", GL_FRAGMENT_SHADER};
+    fragment_shader.load_and_compile();
+    vw::ShaderProgram program{&vertex_shader, &fragment_shader};
+    program.load_and_compile();
+    fragment_shader.gl_delete();
+    vertex_shader.gl_delete();
+    
+    while (!glfwWindowShouldClose(viewer.window))
+    {
+        glfwSwapBuffers(viewer.window);
+        glfwPollEvents();
+        
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        program.use();
+        glBindVertexArray(VAO1);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+
+    return 0;
+}
